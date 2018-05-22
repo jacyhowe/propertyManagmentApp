7091da98b65e8be36de80bedf9105799606ca8bd
jacyhowe <jacyhowe@hotmail.com>
2018-05-21 19:28:25 -0600

    setup project and header HOC

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+ class Header extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Header</h1>
       </div>
     );
   }
 }
+
+export default Header;
