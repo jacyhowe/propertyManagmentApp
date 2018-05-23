cc77f359bb06715da9acfa3ca92e99001bb8f552
jacyhowe <jacyhowe@hotmail.com>
2018-05-22 19:22:32 -0600

    added redux form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
+
+    renderInput(field) {
+        return <input className="form-control" {...field.input} type="field.type"/>
+    }
+
+    handleFormSubmit = function() {
+        
+    }
+
     render() {
+        const { handleSubmit } = this.props;
       return (
         <div>
-          sign in form          
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>             
+                <label htmlFor="email">Email</label>
+                <Field class="email" component={this.renderInput} type="text"/>
+                <label htmlFor="password">Password</label>
+                <Field class="password" component={this.renderInput} type="password"/>
+                <button action="submit" className="btn btn primary">Sign In</button>
+            </form>       
         </div>
       );
     }
   }
   
-  export default Signin;
+  export default reduxForm({form: "signin"})(Signin);
