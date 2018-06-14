b1f155b01405b804c89a8f7af62223f45c039354
jacyhowe <jacyhowe@hotmail.com>
2018-06-13 18:42:24 -0600

    rendered fetched item on the screen

--- src/components/newsletter/newsletter_detail.js
+++ src/components/newsletter/newsletter_detail.js
@@ -9,17 +9,41 @@ class NewsletterDetail extends Component {
         this.props.fetchNewsletterById(this.props.match.params._id) 
     }
 
+    renderFetchedItem() {
+        if(this.props.fetchedItem) {
+            return (
+                <div>
+                    <div>
+                        <img src={this.props.fetchedItem.imageUrl}/>
+                        <h2>{this.props.fetchedItem.title}</h2>
+                    </div>
+                    <p>
+                        {this.props.fetchedItem.body}
+                    </p>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
-            <div>
-                newsletter detail
+            <div className="row">
+                <div className="col-md-3" row>
+                    <div style={{width: '200px', height: '200px', backgroundColor: 'skyblue'}}>
+                        02 JAN 18
+                    </div>                                        
+                </div>
+                <div className="col-md-9">
+                    {this.renderFetchedItem()}
+                </div>
             </div>
         )
     }
 }
 
+
+
 function mapStateToProps(state) {
-    
     return {
         fetchedItem: state.newsletter.fetchedItem
     }
